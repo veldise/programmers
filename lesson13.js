b2f9b64df50ea785effef69bd96cb0349bb68df2
최영철 <veldise@gmail.com>
2023-11-28 21:53:26 +0900

    Update lesson 13

--- lesson13.js
+++ lesson13.js
@@ -1,10 +1,79 @@
 /**
  * 이모티콘 할인행사: https://school.programmers.co.kr/learn/courses/30/lessons/150368
+ * 각 이모티콘별 최적 할인율 구하기 ?
+ *
  */
 
+// SEE: https://velog.io/@proshy/JS순열조합중복순열-구하기
+function permutation(arr, selectNum) {
+  const result = [];
+  if (selectNum === 1) return arr.map((v) => [v]);
+
+  arr.forEach((v, idx, arr) => {
+    const fixed = v;
+    const restArr = arr;
+    const permutationArr = permutation(restArr, selectNum - 1);
+    const combineFix = permutationArr.map((v) => [fixed, ...v]);
+    result.push(...combineFix);
+  });
+  return result;
+}
+
+function calcResult(users, prices) {
+  let cntJoin = 0; // 서비스 가입 수
+  let sumAmount = 0; // 매출액
+
+  for (const [uRate, uPrice] of users) {
+    let purchased = 0;
+
+    for (const [pRate, pPrice] of prices) {
+      if (uRate <= pRate) {
+        // NOTE: emoticon 가격은 100의 배수이므로 100으로 먼저 나누어 부동소수점 발생 방지
+        // x * (1-0.4) = (x/100 * (100-40)/100) * 100 = x/100 * (100-40)
+        let discounted = (pPrice / 100) * (100 - pRate); // 할인된 emoticon 가격
+        purchased += discounted;
+
+        // 구매 비용이 기준 이상이면
+        if (purchased >= uPrice) {
+          purchased = 0; // 구매 모두 취소
+          cntJoin += 1; // 서비스 가입
+          break;
+        }
+      }
+    }
+
+    sumAmount += purchased;
+  }
+
+  return [cntJoin, sumAmount];
+}
+
 function solution(users, emoticons) {
-  var answer = [];
-  return answer;
+  // 할인율은 10/20/30/40% 중 하나
+  const discountRates = [10, 20, 30, 40];
+  const allCaseRates = permutation(discountRates, emoticons.length);
+
+  const results = [];
+  // let answer = null;
+
+  for (const caseRates of allCaseRates) {
+    const pairs = [];
+    for (let i = 0; i < emoticons.length; i++) {
+      pairs.push([caseRates[i], emoticons[i]]);
+    }
+
+    // 서비스 가입 수 우선, 매출액은 그 다음
+    // const result = calcResult(users, pairs);
+    // if (!answer || result[0] > answer[0] || (result[0] === answer[0] && result[1] >= answer[1])) {
+    //   answer = result;
+    // }
+    results.push(calcResult(users, pairs));
+  }
+
+  // 0, 1 순서로 내림차순 정렬
+  return results.sort((a, b) => {
+    return b[0] - a[0] || b[1] - a[1];
+  })[0]; // 가장 큰 값
 }
 
 function solutionOther(users, emoticons) {
