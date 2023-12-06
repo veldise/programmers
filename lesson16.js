ed8cea786631590c857c99fd093f8867156a6ba5
최영철 <veldise@gmail.com>
2023-12-06 14:19:18 +0900

    Update lesson 15, 16

--- lesson16.js
+++ lesson16.js
@@ -3,7 +3,18 @@
  */
 
 function solution(progresses, speeds) {
-  var answer = [];
+  const workDays = progresses.map((p, i) => Math.ceil((100 - p) / speeds[i]));
+  const answer = [];
+  let currWd = workDays[0];
+  let i = 0;
+
+  for (const wd of workDays) {
+    if (currWd < wd) {
+      currWd = wd;
+      i++;
+    }
+    answer[i] = answer[i] ? answer[i] + 1 : 1;
+  }
   return answer;
 }
 
@@ -17,8 +28,12 @@ function solutionOther(progresses, speeds) {
  */
 if (require.main === module) {
   const testCases = [
+    { progresses: [94, 94, 30, 55], speeds: [5, 5, 30, 5], result: [2, 1, 1] },
+    { progresses: [94, 30, 55], speeds: [5, 30, 5], result: [1, 1, 1] },
     { progresses: [93, 30, 55], speeds: [1, 30, 5], result: [2, 1] },
-    { progresses: [95, 90, 99, 99, 80, 99], speeds: [1, 1, 1, 1, 1, 1], result: [1, 3, 2] }
+    { progresses: [93, 30, 30, 55], speeds: [1, 30, 30, 5], result: [3, 1] },
+    { progresses: [95, 90, 99, 99, 80, 99], speeds: [1, 1, 1, 1, 1, 1], result: [1, 3, 2] },
+    { progresses: [99, 97, 55, 0], speeds: [1, 1, 4, 100], result: [1, 1, 2] },
   ];
 
   const success = testCases.every((tc) => {
