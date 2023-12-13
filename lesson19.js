08719a3607fcdd9876e0eba333130008732c95d8
최영철 <veldise@gmail.com>
2023-12-14 07:51:45 +0900

    Update lesson 19, 20

--- lesson19.js
+++ lesson19.js
@@ -3,8 +3,44 @@
  */
 
 function solution(files) {
-  var answer = [];
-  return answer;
+  // 영문 대소문자, 숫자, 공백(" "), 마침표("."), 빼기 부호("-")
+  const re = /^(\D+)(\d{1,5})/;
+
+  return files.sort((fa, fb) => {
+    // 정규식 capturing group
+    // SEE: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Regular_expressions/Capturing_group
+    const ma = re.exec(fa);
+    const mb = re.exec(fb);
+    if (!ma || !mb) {
+      return 0; // arg error 상황
+    }
+
+    let [headA, numberA] = ma.slice(1);
+    let [headB, numberB] = mb.slice(1);
+    // 대소문자 무시
+    headA = headA.toLowerCase();
+    headB = headB.toLowerCase();
+    if (headA !== headB) {
+      return headA > headB ? 1 : -1;
+    } else {
+      return +numberA - +numberB;
+    }
+  });
+}
+
+function solution2(files) {
+  // SEE: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/Collator/Collator
+  const replacer = (file) => {
+    // 비교 기준이 되는 문자열을 제외하고 제거
+    // ex) img1.png -> img1
+    // ex) A-10 Thunderbolt II -> A-10
+    return file.replace(/^(\D+)(\d{1,5}).*/, "$1$2");
+  };
+  const compare = new Intl.Collator(undefined, { sensitivity: "case", numeric: true }).compare;
+
+  return files.sort((fa, fb) => {
+    return compare(replacer(fa), replacer(fb));
+  });
 }
 
 function solutionOther(files) {
@@ -28,8 +64,8 @@ if (require.main === module) {
   ];
 
   const success = testCases.every((tc) => {
-    console.log(solution(tc.input), tc.output);
-    return require("lodash").isEqual(solution(tc.input), tc.output);
+    console.log(solution2(tc.input), tc.output);
+    return require("lodash").isEqual(solution2(tc.input), tc.output);
   });
   console.log(success);
 }
--- lesson20.js
+++ lesson20.js
@@ -3,7 +3,29 @@
  */
 
 function solution(s) {
-  var answer = -1;
+  // 홀수면 올바른 괄호 문자열 불가능
+  if (s.length % 2 === 1) {
+    return 0;
+  }
+
+  let answer = 0;
+  let sLength = s.length;
+
+  while (sLength--) {
+    let rotated = s.slice(sLength) + s.slice(0, sLength);
+
+    // 괄호 종류만큼 반복 제거
+    // ex) {[()]} -> {[]} -> {} ->
+    rotated = rotated
+      .replace(/\(\)|\[\]|\{\}/, "")
+      .replace(/\(\)|\[\]|\{\}/, "")
+      .replace(/\(\)|\[\]|\{\}/, "");
+
+    if (!rotated.length) {
+      answer += 1;
+    }
+  }
+
   return answer;
 }
 
@@ -20,7 +42,10 @@ if (require.main === module) {
     { s: "[](){}", result: 3 },
     { s: "}]()[{", result: 2 },
     { s: "[)(]", result: 0 },
-    { s: "}}}", result: 0 }
+    { s: "}}}", result: 0 },
+
+    { s: "{(}[)]", result: 0 },
+    { s: "{[()]}", result: 1 }
   ];
 
   const success = testCases.every((tc) => {
