5157a53b56503a4302f5c7f3dfc03ab54dcbe8fb
최영철 <veldise@gmail.com>
2023-12-22 10:02:06 +0900

    Update lesson 21

--- lesson21.js
+++ lesson21.js
@@ -2,13 +2,33 @@
  * 가장 큰 정사각형 찾기: https://school.programmers.co.kr/learn/courses/30/lessons/12905
  */
 function solution(board) {
-  var answer = 1234;
-  return answer;
+  const rowLength = board.length;
+  const colLength = board[0].length;
+
+  let answer = 0;
+  for (let r = 1; r < rowLength; r++) {
+    for (let c = 1; c < colLength; c++) {
+      if (board[r][c]) {
+      }
+    }
+  }
+  return answer ** 2;
 }
 
 function solutionOther(board) {
-  var answer = 1234;
-  return answer;
+  const rowLength = board.length;
+  const colLength = board[0].length;
+
+  let answer = 0;
+  for (let r = 1; r < rowLength; r++) {
+    for (let c = 1; c < colLength; c++) {
+      if (board[r][c]) {
+        board[r][c] = Math.min(board[r - 1][c], board[r][c - 1], board[r - 1][c - 1]) + 1;
+        answer = Math.max(board[r][c], answer);
+      }
+    }
+  }
+  return answer ** 2;
 }
 
 /**
@@ -18,10 +38,10 @@ if (require.main === module) {
   const testCases = [
     {
       board: [
-        [0, 1, 1, 1],
-        [1, 1, 1, 1],
-        [1, 1, 1, 1],
-        [0, 0, 1, 0]
+        [0, 1, 1, 1], // 1, 3 - 3
+        [1, 1, 1, 1], // 0, 3 - 4
+        [1, 1, 1, 1], // 0, 3 - 4
+        [0, 0, 1, 0] // 2, 2 - 1
       ],
       answer: 9
     },
@@ -31,6 +51,49 @@ if (require.main === module) {
         [1, 1, 1, 1]
       ],
       answer: 4
+    },
+    {
+      board: [
+        [1, 1, 0, 0],
+        [1, 1, 1, 1]
+      ],
+      answer: 4
+    },
+    {
+      board: [
+        [0, 0, 0, 1],
+        [1, 1, 1, 1],
+        [1, 1, 1, 1],
+        [1, 0, 1, 0]
+      ],
+      answer: 4
+    },
+    {
+      board: [
+        [1, 0, 1, 1],
+        [0, 0, 1, 1],
+        [1, 1, 1, 1],
+        [1, 0, 1, 0]
+      ],
+      answer: 4
+    },
+    {
+      board: [
+        [0, 0, 1, 1],
+        [0, 1, 0, 0],
+        [0, 0, 1, 0],
+        [0, 1, 1, 0]
+      ],
+      answer: 1
+    },
+    {
+      board: [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+      ],
+      answer: 0
     }
   ];
 
