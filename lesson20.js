2e5b4d467df3ebd6623b33e0557ddd717026913f
최영철 <veldise@gmail.com>
2024-01-10 16:31:29 +0900

    Fixed lesson 20

--- lesson20.js
+++ lesson20.js
@@ -13,16 +13,25 @@ function solution(s) {
 
   while (sLength--) {
     let rotated = s.slice(sLength) + s.slice(0, sLength);
+    const n = s.length / 2;
 
-    // 괄호 종류만큼 반복 제거
-    // ex) {[()]} -> {[]} -> {} ->
-    rotated = rotated
-      .replace(/\(\)|\[\]|\{\}/, "")
-      .replace(/\(\)|\[\]|\{\}/, "")
-      .replace(/\(\)|\[\]|\{\}/, "");
+    // NOTE: 경우에 따라 500번 순회하므로 매우 느림. 다르게 풀어야 함.
+    for (let i = 0; i < n; i++) {
+      // 괄호 종류만큼 반복 제거
+      // ex) {[()]} -> {[]} -> {} ->
+      let rst = rotated.replace(/\(\)|\[\]|\{\}/g, "");
 
-    if (!rotated.length) {
-      answer += 1;
+      // 모두 제거되었으면 올바른 문자열
+      if (!rst.length) {
+        answer += 1;
+        break;
+      }
+      // 제거한게 하나도 없으면 올바르지 않음
+      else if (rst.length === rotated.length) {
+        break;
+      }
+
+      rotated = rst;
     }
   }
 
@@ -45,7 +54,10 @@ if (require.main === module) {
     { s: "}}}", result: 0 },
 
     { s: "{(}[)]", result: 0 },
-    { s: "{[()]}", result: 1 }
+    { s: "{[()]}", result: 1 },
+    { s: "()(", result: 0 },
+    { s: "(", result: 0 },
+    { s: "{{{{{{", result: 0 }
   ];
 
   const success = testCases.every((tc) => {
