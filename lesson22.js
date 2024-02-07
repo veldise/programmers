f02eda1aa352855cab606c1f3dd4c59c84094f84
최영철 <veldise@gmail.com>
2024-02-08 08:11:58 +0900

    Update lesson 22, 23

--- lesson22.js
+++ lesson22.js
@@ -1,47 +1,44 @@
 /**
- * 가장 많이 받은 선물: https://school.programmers.co.kr/learn/courses/30/lessons/258712
+ * [3차] 방금그곡: https://school.programmers.co.kr/learn/courses/30/lessons/17683
  */
-function solution(friends, gifts) {
-  const giftMap = {};
-  const coefMap = {};
-  const answerMap = {};
-
-  // 초기화
-  friends.forEach((f1) => {
-    giftMap[f1] = {};
-    friends.forEach((f2) => {
-      giftMap[f1][f2] = 0;
-    });
-    coefMap[f1] = 0;
-    answerMap[f1] = 0;
-  });
-
-  // 집계
-  gifts.forEach((log) => {
-    const [g, r] = log.split(" ");
-    giftMap[g][r] = giftMap[g][r] + 1;
-    coefMap[g] = coefMap[g] + 1;
-    coefMap[r] = coefMap[r] - 1;
-  });
-
-  // 계산
-  friends.forEach((g) => {
-    friends.forEach((r) => {
-      if (g === r || giftMap[g][r] < giftMap[r][g]) {
-        return;
-      }
-
-      if (giftMap[g][r] > giftMap[r][g] || coefMap[g] > coefMap[r]) {
-        answerMap[g] += 1;
-      }
-    });
-  });
-
-  return Math.max(...Object.values(answerMap));
+function time2minute(time) {
+  const [h, m] = time.split(":");
+  return h * 60 + +m;
 }
 
-function solutionOther(friends, gifts) {
-  var answer = 0;
+function solution(m, musicinfos) {
+  let foundTitle = null;
+
+  const reSheet = /C#?|D#?|E|F#?|G#?|A#?|B/g;
+  const strRememberSheet = m.match(reSheet).join(",") + ","; // 'AB#C' -> 'A,B#,C,'
+
+  for (const info of musicinfos) {
+    const [sta, eta, title, sheetStr] = info.split(",");
+    const sheet = sheetStr.match(reSheet);
+
+    let playingTime = time2minute(eta) - time2minute(sta);
+    let playingSheet = [];
+    do {
+      // TODO: String.prototype.repeat()
+      const sliced = sheet.slice(0, playingTime);
+      playingSheet = playingSheet.concat(sliced);
+      playingTime -= sliced.length;
+    } while (playingTime > 0);
+
+    const strPlayingSheet = playingSheet.join(",") + ",";
+    if (strPlayingSheet.indexOf(strRememberSheet) !== -1) {
+      foundTitle = title;
+    }
+    // TODO: 조건이 일치하는 음악이 여러 개일 때에는 라디오에서
+    // 재생된 시간이 제일 긴 음악 제목을 반환한다.
+    // 재생된 시간도 같을 경우 먼저 입력된 음악 제목을 반환한다.
+  }
+
+  return foundTitle || "(None)";
+}
+
+function solutionOther(m, musicinfos) {
+  var answer = "";
   return answer;
 }
 
@@ -50,31 +47,14 @@ function solutionOther(friends, gifts) {
  */
 if (require.main === module) {
   const testCases = [
-    {
-      friends: ["muzi", "ryan", "frodo", "neo"],
-      gifts: [
-        "muzi frodo",
-        "muzi frodo",
-        "ryan muzi",
-        "ryan muzi",
-        "ryan muzi",
-        "frodo muzi",
-        "frodo ryan",
-        "neo muzi"
-      ],
-      result: 2
-    },
-    {
-      friends: ["joy", "brad", "alessandro", "conan", "david"],
-      gifts: ["alessandro brad", "alessandro joy", "alessandro conan", "david alessandro", "alessandro david"],
-      result: 4
-    },
-    { friends: ["a", "b", "c"], gifts: ["a b", "b a", "c a", "a c", "a c", "c a"], result: 0 }
+    { m: "ABCDEFG", musicinfos: ["12:00,12:14,HELLO,CDEFGAB", "13:00,13:05,WORLD,ABCDEF"], answer: "HELLO" },
+    { m: "CC#BCC#BCC#BCC#B", musicinfos: ["03:00,03:30,FOO,CC#B", "04:00,04:08,BAR,CC#BCC#BCC#B"], answer: "FOO" },
+    { m: "ABC", musicinfos: ["12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF"], answer: "WORLD" }
   ];
 
   const success = testCases.every((tc) => {
-    console.log(solution(tc.friends, tc.gifts), tc.result);
-    return require("lodash").isEqual(solution(tc.friends, tc.gifts), tc.result);
+    console.log(solution(tc.m, tc.musicinfos), tc.answer);
+    return require("lodash").isEqual(solution(tc.m, tc.musicinfos), tc.answer);
   });
   console.log(success);
 }
