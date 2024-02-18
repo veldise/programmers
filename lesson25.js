141996480cfa77a506c5191e334f1f08dae25598
최영철 <veldise@gmail.com>
2024-02-18 18:35:10 +0900

    Update lesson 25

--- lesson25.js
+++ lesson25.js
@@ -7,37 +7,49 @@ function solution(picks, minerals) {
     iron: { diamond: 5, iron: 1, stone: 1 },
     stone: { diamond: 25, iron: 5, stone: 1 }
   };
-  let fatigue = 0;
-  let currPick = null;
-  let cntUsedPick = 5;
-
   let [dia, iron, stone] = picks;
+  let fatigue = 0;
 
-  for (const mineral of minerals) {
-    // if (!dia && !iron && !stone) {
-    //   break;
-    // }
-    if (!currPick) {
-      if (dia) {
-        currPick = "dia";
-        dia--;
-      } else if (iron) {
-        currPick = "iron";
-        iron--;
-      } else if (stone) {
-        currPick = "stone";
-        stone--;
-      } else {
-        break;
-      }
+  // 곡괭이 개수 * 5 만큼만 캘 수 있다.
+  const sum = picks.reduce((acc, cur) => acc + cur, 0);
+  minerals = minerals.slice(0, sum * 5);
+
+  // 5개씩 분할하며 광물 종류별 개수 세기
+  const mineralsCntList = [];
+
+  for (let i = 0; i < minerals.length; i+=5) {
+    const mnrs = minerals.slice(i, i + 5);
+
+    const countMap = { diamond: 0, iron: 0, stone: 0 };
+    for (const mnr of mnrs) {
+      countMap[mnr] += 1;
     }
 
-    fatigue += fatigueMap[currPick][mineral] || 0;
-    console.log(currPick, mineral, fatigue);
+    mineralsCntList.push(countMap);
+  }
 
-    if (!--cntUsedPick) {
-      cntUsedPick = 5;
-      currPick = null;
+  // 광물 종류별 & 개수 순으로 정렬 (피로도 높은 순으로, diamond > iron > stone)
+  mineralsCntList.sort(function (idxs1, idxs2) {
+    return idxs2.diamond - idxs1.diamond || idxs2.iron - idxs1.iron || idxs2.stone - idxs1.stone;
+  });
+
+  // 피로도 계산 (dia > iron > stone 순으로 사용)
+  for (const countMap of mineralsCntList) {
+    if (dia) {
+      dia--;
+      for (const [mnr, count] of Object.entries(countMap)) {
+        fatigue += fatigueMap.dia[mnr] * count;
+      }
+    } else if (iron) {
+      iron--;
+      for (const [mnr, count] of Object.entries(countMap)) {
+        fatigue += fatigueMap.iron[mnr] * count;
+      }
+    } else if (stone) {
+      stone--;
+      for (const [mnr, count] of Object.entries(countMap)) {
+        fatigue += fatigueMap.stone[mnr] * count;
+      }
     }
   }
 
@@ -75,6 +87,32 @@ if (require.main === module) {
         "diamond"
       ],
       result: 50
+    },
+    {
+      picks: [2, 1, 0],
+      minerals: [
+        "diamond",
+        "diamond",
+        "diamond",
+        "diamond",
+        "diamond",
+        "iron",
+        "iron",
+        "iron",
+        "iron",
+        "iron",
+        "diamond",
+        "diamond",
+        "diamond",
+        "diamond",
+        "diamond"
+      ],
+      result: 15
+    },
+    {
+      picks: [0, 1, 0],
+      minerals: ["diamond", "iron", "iron", "iron", "iron", "diamond", "diamond", "iron", "iron", "iron"],
+      result: 9
     }
   ];
 
