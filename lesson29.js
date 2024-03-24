c5c9431709a63cdee1d1b7fb55d256cf12c5bd9e
최영철 <veldise@gmail.com>
2024-03-24 22:54:03 +0900

    Update lesson 29

--- lesson29.js
+++ lesson29.js
@@ -1,9 +1,44 @@
 /**
  * [단속카메라](https://school.programmers.co.kr/learn/courses/30/lessons/42884)
  */
+
+function isLineIntersection(la, lb) {
+  return la[1] >= lb[0];
+}
+function getLineIntersection(la, lb) {
+  return isLineIntersection(la, lb) ? [Math.max(la[0], lb[0]), Math.min(la[1], lb[1])] : null;
+}
+
 function solution(routes) {
-  var answer = 0;
-  return answer;
+  if (routes.length <= 1) {
+    return routes.length;
+  }
+
+  routes = routes.sort((a, b) => a[0] - b[0] || a[1] - b[1]);
+
+  let cntCross = 0;
+
+  while (routes.length) {
+    // 교차 route 찾기
+    let line = routes.shift();
+    const indexes = [];
+
+    for (let i = 0; i < routes.length; i++) {
+      const rst = getLineIntersection(line, routes[i]);
+      if (rst) {
+        line = rst;
+        indexes.push(i);
+      }
+    }
+    // 교차 route 일괄 제거
+    indexes.reverse().forEach((index) => {
+      routes.splice(index, 1);
+    });
+
+    cntCross++;
+  }
+
+  return cntCross;
 }
 
 /**
@@ -182,6 +217,15 @@ if (require.main === module) {
         [2, 3],
         [3, 4],
         [4, 6]
+      ],
+      result: 2
+    },
+    {
+      routes: [
+        [0, 10],
+        [1, 10],
+        [2, 3],
+        [4, 5]
       ],
       result: 2
     }
