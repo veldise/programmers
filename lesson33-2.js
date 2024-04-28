19ac33d7abeac019bcdbacf780883ec320526a8e
최영철 <veldise@gmail.com>
2024-04-28 21:31:18 +0900

    Update lesson 33 - 2, 3

--- lesson33-2.js
+++ lesson33-2.js
@@ -3,8 +3,37 @@
  */
 
 function solution(answers) {
-  var answer = [];
-  return answer;
+  const aSolve = [1, 2, 3, 4, 5];
+  const bSolve = [2, 1, 2, 3, 2, 4, 2, 5];
+  const cSolve = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+
+  let aScore = 0;
+  let bScore = 0;
+  let cScore = 0;
+
+  for (let i = 0; i < answers.length; i++) {
+    const answer = answers[i];
+
+    if (answer === aSolve[i % aSolve.length]) {
+      aScore++;
+    }
+    if (answer === bSolve[i % bSolve.length]) {
+      bScore++;
+    }
+    if (answer === cSolve[i % cSolve.length]) {
+      cScore++;
+    }
+  }
+
+  const scoreArr = [
+    [1, aScore],
+    [2, bScore],
+    [3, cScore]
+  ].sort((a, b) => b[1] - a[1]);
+
+  const maxScore = scoreArr[0][1];
+
+  return scoreArr.filter((arr) => maxScore === arr[1]).map((arr) => arr[0]);
 }
 
 /**
@@ -13,7 +42,10 @@ function solution(answers) {
 if (require.main === module) {
   const testCases = [
     { answers: [1, 2, 3, 4, 5], answer: [1] },
-    { answers: [1, 3, 2, 4, 2], answer: [1, 2, 3] }
+    { answers: [1, 3, 2, 4, 2], answer: [1, 2, 3] },
+    { answers: [1, 1, 1, 1, 1, 1], answer: [1, 3] },
+    { answers: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5], answer: [1, 3] },
+    { answers: [3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5], answer: [3] }
   ];
 
   testCases.forEach((tc) => {
--- lesson33-3.js
+++ lesson33-3.js
@@ -3,8 +3,14 @@
  */
 
 function solution(food) {
-  var answer = "";
-  return answer;
+  let answer = ["0"];
+  for (let i = food.length; i >= 1; i--) {
+    for (let n = 1; n < food[i]; n += 2) {
+      answer.unshift(i);
+      answer.push(i);
+    }
+  }
+  return answer.join("");
 }
 
 /**
@@ -13,7 +19,8 @@ function solution(food) {
 if (require.main === module) {
   const testCases = [
     { food: [1, 3, 4, 6], answer: "1223330333221" },
-    { food: [1, 7, 1, 2], answer: "111303111" }
+    { food: [1, 7, 1, 2], answer: "111303111" },
+    { food: [1, 3, 2, 3], answer: "1230321" }
   ];
 
   testCases.forEach((tc) => {
