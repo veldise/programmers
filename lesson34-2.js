8a130fb1e09348b8110a86917ff61013d4ab682a
최영철 <veldise@gmail.com>
2024-05-18 18:37:42 +0900

    Update lesson 34

--- lesson34-2.js
+++ lesson34-2.js
@@ -1,10 +1,30 @@
 /**
  * [실패율](https://school.programmers.co.kr/learn/courses/30/lessons/42889)
+ * +8
  */
 
 function solution(N, stages) {
-  var answer = [];
-  return answer;
+  const stageMap = {};
+  const clearMap = {};
+  for (const stage of stages) {
+    for (let i = 1; i <= stage; i++) {
+      if (i <= N) {
+        stageMap[i] = (stageMap[i] || 0) + 1;
+      }
+      if (i > 1) {
+        clearMap[i - 1] = (clearMap[i - 1] || 0) + 1;
+      }
+    }
+  }
+
+  const failRateMap = {};
+  for (let i = 1; i <= N; i++) {
+    failRateMap[i] = (stageMap[i] - (clearMap[i] || 0)) / stageMap[i];
+  }
+
+  return Object.entries(failRateMap)
+    .sort((a, b) => b[1] - a[1])
+    .map((a) => +a[0]);
 }
 
 /**
